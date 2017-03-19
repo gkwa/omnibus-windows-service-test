c58650f03c29b369254acbd2b24838596e3d4d4f
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-19 14:50:11 -0700

    omnibus new windows_service_test --msi-assets

---
+++ omnibus-windows_service_test/config/projects/windows_service_test.rb
@@ -0,0 +1,28 @@
+#
+# Copyright 2017 YOUR NAME
+#
+# All Rights Reserved.
+#
+
+name "windows_service_test"
+maintainer "CHANGE ME"
+homepage "https://CHANGE-ME.com"
+
+# Defaults to C:/windows_service_test on Windows
+# and /opt/windows_service_test on all other platforms
+install_dir "#{default_root}/#{name}"
+
+build_version Omnibus::BuildVersion.semver
+build_iteration 1
+
+# Creates required build directories
+dependency "preparation"
+
+# windows_service_test dependencies/components
+# dependency "somedep"
+
+# Version manifest file
+dependency "version-manifest"
+
+exclude "**/.git"
+exclude "**/bundler/git"
